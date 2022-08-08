be19eb11bfb1231309b658437c27b506cb70f39c
OhKwang2 <dhrhkd007@naver.com>
2022-08-08 21:51:44 +0900

    2022.08.08 - Security 첫 시간

---
+++ src/main/java/com/study/security_kok/SecurityKokApplication.java
@@ -0,0 +1,13 @@
+package com.study.security_kok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityKokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityKokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/security_kok/SecurityKokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security_kok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityKokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
